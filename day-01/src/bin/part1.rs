0e4898eb0a269ff78d7859976b877f52ddc23ccb
Vincent HAYS <vincenthays95@gmail.com>
2023-12-01 16:42:16 +0100

    clean code

---
+++ day-01/src/bin/part1.rs
@@ -0,0 +1,23 @@
+use regex::Regex;
+use rayon::prelude::*;
+
+fn main() {
+    let re = Regex::new(r"\D").unwrap();
+
+    let sum: u32 =
+        include_str!("input1.txt")
+        .par_split('\n')
+        .map(|l| {
+            let clean = re.replace_all(l, "");
+            let mut chars = clean.chars();
+            let first = chars.next().unwrap();
+            let last = chars.next_back().unwrap_or(first);
+
+            let first = first.to_digit(10).unwrap();
+            let last = last.to_digit(10).unwrap();
+
+            first * 10 + last
+        }).sum();
+
+    println!("{}", sum);
+}
