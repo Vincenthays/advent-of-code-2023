66c8e62e8cba5cb5a5dc78cf47c239f79c6154e2
Vincent HAYS <vincenthays95@gmail.com>
2023-12-02 20:39:45 +0100

    clean code

--- day-01/src/bin/part2.rs
+++ day-01/src/bin/part2.rs
@@ -2,21 +2,6 @@ use regex::Regex;
 use rayon::prelude::*;
 
 
-fn to_u32(s: &str) -> u32 {
-    match s {
-        "1" | "one" | "eno" => 1,
-        "2" | "two" | "owt" => 2,
-        "3" | "three" | "eerht" => 3,
-        "4" | "four" | "ruof" => 4,
-        "5" | "five" | "evif" => 5,
-        "6" | "six" | "xis" => 6,
-        "7" | "seven" | "neves" => 7,
-        "8" | "eight" | "thgie" => 8,
-        "9" | "nine" | "enin" => 9,
-        _ => 0
-    }
-}
-
 fn main() {
     let re1 = Regex::new(r"(\d|one|two|three|four|five|six|seven|eight|nine)").unwrap();
     let re2 = Regex::new(r"(\d|eno|owt|eerht|ruof|evif|xis|neves|thgie|enin)").unwrap();
@@ -34,4 +19,19 @@ fn main() {
         .sum::<u32>();
 
     println!("{}", sum);
+}
+
+fn to_u32(s: &str) -> u32 {
+    match s {
+        "1" | "one" | "eno" => 1,
+        "2" | "two" | "owt" => 2,
+        "3" | "three" | "eerht" => 3,
+        "4" | "four" | "ruof" => 4,
+        "5" | "five" | "evif" => 5,
+        "6" | "six" | "xis" => 6,
+        "7" | "seven" | "neves" => 7,
+        "8" | "eight" | "thgie" => 8,
+        "9" | "nine" | "enin" => 9,
+        _ => 0
+    }
 }
