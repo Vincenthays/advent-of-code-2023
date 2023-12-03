30dd7ac2e165a0129505135d4a9801c1a9ca33bb
Vincent HAYS <vincenthays95@gmail.com>
2023-12-03 21:42:35 +0100

    clean code

--- day-03/src/bin/part1.rs
+++ day-03/src/bin/part1.rs
@@ -15,17 +15,10 @@ fn main() {
                     (m.start()..m.end()+1)
                     .any(|j| check_diagonal_neighbors(&input, i, j));
 
-                if has_diagonal_neighbors {
-                    return true
-                }
-
-                // before || after
-                if check_symbol(input[i].chars().nth(m.start()-1))
-                    || check_symbol(input[i].chars().nth(m.end()+1)) {
-                    return true
-                }
-
-                return false
+                return
+                    has_diagonal_neighbors
+                    || check_line_pos(&input, i, m.start()-1)
+                    || check_line_pos(&input, i, m.end()+1);
             })
             .map(|m| {
                 println!("{:?}", m);
@@ -39,23 +32,23 @@ fn main() {
 
 fn check_diagonal_neighbors(input: &Vec<&str>, i: usize, j: usize) -> bool {
     return check_line_pos(input, i+1, j) // line up
-        || check_line_pos(input, i, j)   // same line
         || check_line_pos(input, i-1, j) // line down
 }
 
 fn check_line_pos(input: &Vec<&str>, line_index: usize, char_index: usize) -> bool {
     if let Some(line) = input.get(line_index) {
-        return check_symbol(line.chars().nth(char_index-1)) // before
-            || check_symbol(line.chars().nth(char_index+1)) // after
+        let line = line.chars().collect::<Vec<char>>();
+        return check_symbol(line.get(char_index-1)) // before
+            || check_symbol(line.get(char_index+1)) // after
     }
     return false
 }
 
-fn check_symbol(c: Option<char>) -> bool {
+fn check_symbol(c: Option<&char>) -> bool {
     if let Some(c) = c {
         return Regex::new(r"[^(\d|\\.)]")
             .unwrap()
-            .is_match(&c.to_string());
+            .is_match(&c.to_string())
     }
     return false
 }
