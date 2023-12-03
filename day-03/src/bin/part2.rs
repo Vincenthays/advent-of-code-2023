14d0b7cc6adf727f918bacc0c3b2d3dd86e372dc
Vincent HAYS <vincenthays95@gmail.com>
2023-12-03 23:38:45 +0100

    clean code

--- day-03/src/bin/part2.rs
+++ day-03/src/bin/part2.rs
@@ -1,32 +1,47 @@
-use rayon::prelude::*;
-use std::collections::HashMap;
+use regex::Regex;
 
 fn main() {
+    let re = Regex::new(r"\*").unwrap();
+    let input = include_str!("input_test.txt")
+        .lines()
+        .collect::<Vec<_>>();
+
     let sum =
-        include_str!("input.txt")
-        .par_lines()
-        .map(|l| l
-            .split_once(": ")
+        (0..input.len())
+        .map(|i| re
+            .find_iter(input[i])
+            .map(|m| find_neighbors(&input, i, m.start()))
+            .filter(|n| n.len() == 2)
+            .map(|n| n[0] * n[1])
+            .sum()
+        ).sum();
+
+    println!("{:?}", sum);
+}
+
+fn find_neighbors(input: &Vec<&str>, i: usize, j: usize) -> Vec<u32> {
+    return [
+        check_line_pos(input, i+1, j),
+        check_line_pos(input, i, j),
+        check_line_pos(input, i-1, j),
+    ].concat()
+}
+
+fn check_line_pos(input: &Vec<&str>, line_index: usize, char_index: usize) -> Vec<u32> {
+    if let Some(line) = input.get(line_index) {
+        return Regex::new(r"\d+")
             .unwrap()
-            .1
-            .split("; ")
-            .flat_map(|set| set.split(", "))
-            .fold(HashMap::new(), |mut acc, color| {
-                let (number, color) = color.split_once(' ').unwrap();
-                let number = number.parse::<u32>().unwrap();
-
-                acc
-                    .entry(color)
-                    .and_modify(|v| *v = std::cmp::max(number, *v))
-                    .or_insert(number);
-
-                acc
+            .find_iter(line)
+            .filter(|m| {
+                for i in m.start()-1..m.end()+1 {
+                    if i == char_index {
+                        return true
+                    }
+                }
+                return false
             })
-            .into_values()
-            .reduce(|acc, v| acc * v)
-            .unwrap()
-        )
-        .sum::<u32>();
-
-    println!("{}", sum);
+            .map(|m| m.as_str().parse())
+            .collect()
+    }
+    return vec![];
 }
