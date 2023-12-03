abd73d51d1d50e1a45de53d2c2418c63a8375c69
Vincent HAYS <vincenthays95@gmail.com>
2023-12-03 11:24:19 +0100

    day3

---
+++ day-03/src/bin/part1.rs
@@ -0,0 +1,64 @@
+use regex::Regex;
+
+const RE_SYMBOL: Regex = Regex::new(r"[^(\d|\\.)]").unwrap();
+
+fn main() {
+    let re = Regex::new(r"\d+").unwrap();
+    let input = include_str!("input_test.txt")
+        .lines()
+        .collect::<Vec<_>>();
+
+    for i in 0..input.len() {
+        /// line loop
+        let m = re
+            .find_iter(input[i])
+            .filter(|m| {
+                let has_diagonal_neighbors =
+                    (m.start()..m.end()+1)
+                    .any(|j| check_diagonal_neighbors(&input, i, j));
+
+                if has_diagonal_neighbors {
+                    return true
+                }
+
+                /// check symb
+                if input[i].get(m.start()-1) == Some(true) {
+                    return true;
+                }
+
+                if input[i].get(m.end()+1) == Some(true) {
+                    return true;
+                }
+
+                return false
+            })
+            .collect::<Vec<_>>();
+    }
+}
+
+fn check_diagonal_neighbors(input: &Vec<&str>, i: usize, j: usize) -> bool {
+    /// line up
+    if let Some(line) = input.get(i-1) {
+        /// up left || up right
+        if check_symbol(line.get(j-1)) || check_symbol(line.get(j+1)) {
+            return true;
+        }
+    }
+
+    /// line down
+    if let Some(line) = input.get(i+1) {
+        /// down left || down right
+        if check_symbol(line.get(j-1)) || check_symbol(line.get(j+1)) {
+            return true;
+        }
+    }
+
+    return false
+}
+
+fn check_symbol(c: Option<&str>) -> bool {
+    if let Some(c) = c {
+        return RE_SYMBOL.is_match(c);
+    }
+    return false;
+}
---
+++ day-03/src/bin/part2.rs
@@ -0,0 +1,32 @@
+use rayon::prelude::*;
+use std::collections::HashMap;
+
+fn main() {
+    let sum =
+        include_str!("input.txt")
+        .par_lines()
+        .map(|l| l
+            .split_once(": ")
+            .unwrap()
+            .1
+            .split("; ")
+            .flat_map(|set| set.split(", "))
+            .fold(HashMap::new(), |mut acc, color| {
+                let (number, color) = color.split_once(' ').unwrap();
+                let number = number.parse::<u32>().unwrap();
+
+                acc
+                    .entry(color)
+                    .and_modify(|v| *v = std::cmp::max(number, *v))
+                    .or_insert(number);
+
+                acc
+            })
+            .into_values()
+            .reduce(|acc, v| acc * v)
+            .unwrap()
+        )
+        .sum::<u32>();
+
+    println!("{}", sum);
+}
