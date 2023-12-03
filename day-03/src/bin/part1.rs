86eb9036c83160fb69067aaba7cd7865bcb98d9b
Vincent HAYS <vincenthays95@gmail.com>
2023-12-03 22:13:29 +0100

    clean code

--- day-03/src/bin/part1.rs
+++ day-03/src/bin/part1.rs
@@ -6,28 +6,23 @@ fn main() {
         .lines()
         .collect::<Vec<_>>();
 
-    for i in 0..input.len() {
-        // line loop
-        let m = re
+    let sum = (0..input.len())
+        .map(|i| re
             .find_iter(input[i])
             .filter(|m| {
-                let has_diagonal_neighbors =
-                    (m.start()..m.end()+1)
+                let has_diagonal_neighbors = (m.start()..=m.end())
                     .any(|j| check_diagonal_neighbors(&input, i, j));
 
-                return
-                    has_diagonal_neighbors
+                return has_diagonal_neighbors
                     || check_line_pos(&input, i, m.start()-1)
                     || check_line_pos(&input, i, m.end()+1);
             })
-            .map(|m| {
-                println!("{:?}", m);
-                m
-            })
-            .collect::<Vec<_>>();
+            .map(|m| m.as_str().parse::<u32>().unwrap())
+            .sum::<u32>()
+        )
+        .sum::<u32>();
 
-        println!("{:?}", m);
-    }
+    println!("{}", sum);
 }
 
 fn check_diagonal_neighbors(input: &Vec<&str>, i: usize, j: usize) -> bool {
@@ -37,14 +32,13 @@ fn check_diagonal_neighbors(input: &Vec<&str>, i: usize, j: usize) -> bool {
 
 fn check_line_pos(input: &Vec<&str>, line_index: usize, char_index: usize) -> bool {
     if let Some(line) = input.get(line_index) {
-        let line = line.chars().collect::<Vec<char>>();
-        return check_symbol(line.get(char_index-1)) // before
-            || check_symbol(line.get(char_index+1)) // after
+        return check_symbol(line.chars().nth(char_index-1)) // before
+            || check_symbol(line.chars().nth(char_index+1)) // after
     }
     return false
 }
 
-fn check_symbol(c: Option<&char>) -> bool {
+fn check_symbol(c: Option<char>) -> bool {
     if let Some(c) = c {
         return Regex::new(r"[^(\d|\\.)]")
             .unwrap()
