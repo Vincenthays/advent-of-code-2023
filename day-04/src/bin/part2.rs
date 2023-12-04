1e0c0000fb0c3beb839c302b52d7ce64f69a5798
Vincent HAYS <vincenthays95@gmail.com>
2023-12-04 19:32:52 +0100

    clean code

--- day-04/src/bin/part2.rs
+++ day-04/src/bin/part2.rs
@@ -3,8 +3,8 @@ use regex::Regex;
 fn main() {
     let re = Regex::new(r"\s+").unwrap();
 
-    let sum =
-        include_str!("input.txt")
+    let winning_count =
+        include_str!("input_test.txt")
         .lines()
         .map(|l| {
             let (_, l) = l.split_once(": ").unwrap();
@@ -13,12 +13,15 @@ fn main() {
             let winning = re.split(winning.trim()).map(|n| n.parse().unwrap()).collect::<Vec<u32>>();
             let have = re.split(have.trim()).map(|n| n.parse().unwrap()).collect::<Vec<u32>>();
 
-            have
+            let winning_count = have
                 .into_iter()
                 .filter_map(|n| if winning.contains(&n) { Some(n) } else { None })
-                .fold(0, |acc, _| if acc == 0 { 1 } else { acc * 2 })
-        })
-        .sum::<u32>();
+                .count();
 
-    println!("{:?}", sum);
+            (winning_count,
+             (0..winning_count).fold(0, |acc, _| if acc == 0 { 1 } else { acc * 2 }))
+        })
+        .collect::<Vec<_>>();
+
+    println!("{winning_count:?}");
 }
