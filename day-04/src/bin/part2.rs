7981927799c2591c280ee5106fa7facf1c5826d4
Vincent HAYS <vincenthays95@gmail.com>
2023-12-04 15:31:01 +0100

    clean code

--- day-04/src/bin/part2.rs
+++ day-04/src/bin/part2.rs
@@ -1,47 +1,24 @@
 use regex::Regex;
 
 fn main() {
-    let re = Regex::new(r"\*").unwrap();
-    let input = include_str!("input.txt")
-        .lines()
-        .collect::<Vec<_>>();
+    let re = Regex::new(r"\s+").unwrap();
 
     let sum =
-        (0..input.len())
-        .map(|i| re
-            .find_iter(input[i])
-            .map(|m| find_neighbors(&input, i, m.end()))
-            .filter(|n| n.len() == 2)
-            .map(|n| n[0] * n[1])
-            .sum::<u32>()
-        ).sum::<u32>();
+        include_str!("input.txt")
+            .lines()
+            .map(|l| {
+                let (_, l) = l.split_once(": ").unwrap();
+                let (winning, have) = l.split_once(" | ").unwrap();
+
+                let winning = re.split(winning.trim()).map(|n| n.parse().unwrap()).collect::<Vec<u32>>();
+                let have = re.split(have.trim()).map(|n| n.parse().unwrap()).collect::<Vec<u32>>();
+
+                have
+                    .into_iter()
+                    .filter_map(|n| if winning.contains(&n) { Some(n) } else { None })
+                    .fold(0, |acc, _| if acc == 0 { 1 } else { acc * 2 })
+            })
+            .sum::<u32>();
 
     println!("{:?}", sum);
-}
-
-fn find_neighbors(input: &Vec<&str>, i: usize, j: usize) -> Vec<u32> {
-    return [
-        check_line_pos(input, i+1, j),
-        check_line_pos(input, i, j),
-        check_line_pos(input, i-1, j),
-    ].concat()
-}
-
-fn check_line_pos(input: &Vec<&str>, line_index: usize, char_index: usize) -> Vec<u32> {
-    if let Some(line) = input.get(line_index) {
-        return Regex::new(r"\d+")
-            .unwrap()
-            .find_iter(line)
-            .filter(|m| {
-                for i in m.start()..m.end()+2 {
-                    if i == char_index {
-                        return true
-                    }
-                }
-                return false
-            })
-            .map(|m| m.as_str().parse().unwrap())
-            .collect()
-    }
-    return vec![];
 }
