d8c7a8160c62de73dc7cf3900b9446d3b23f0100
Vincent HAYS <vincenthays95@gmail.com>
2023-12-04 16:11:48 +0100

    clean code

--- day-04/src/bin/part2.rs
+++ day-04/src/bin/part2.rs
@@ -5,20 +5,20 @@ fn main() {
 
     let sum =
         include_str!("input.txt")
-            .lines()
-            .map(|l| {
-                let (_, l) = l.split_once(": ").unwrap();
-                let (winning, have) = l.split_once(" | ").unwrap();
+        .lines()
+        .map(|l| {
+            let (_, l) = l.split_once(": ").unwrap();
+            let (winning, have) = l.split_once(" | ").unwrap();
 
-                let winning = re.split(winning.trim()).map(|n| n.parse().unwrap()).collect::<Vec<u32>>();
-                let have = re.split(have.trim()).map(|n| n.parse().unwrap()).collect::<Vec<u32>>();
+            let winning = re.split(winning.trim()).map(|n| n.parse().unwrap()).collect::<Vec<u32>>();
+            let have = re.split(have.trim()).map(|n| n.parse().unwrap()).collect::<Vec<u32>>();
 
-                have
-                    .into_iter()
-                    .filter_map(|n| if winning.contains(&n) { Some(n) } else { None })
-                    .fold(0, |acc, _| if acc == 0 { 1 } else { acc * 2 })
-            })
-            .sum::<u32>();
+            have
+                .into_iter()
+                .filter_map(|n| if winning.contains(&n) { Some(n) } else { None })
+                .fold(0, |acc, _| if acc == 0 { 1 } else { acc * 2 })
+        })
+        .sum::<u32>();
 
     println!("{:?}", sum);
 }
