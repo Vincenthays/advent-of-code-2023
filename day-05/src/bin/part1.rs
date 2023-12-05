3744c806fd82344a560d49b1267f1957e8649b65
Vincent HAYS <vincenthays95@gmail.com>
2023-12-05 20:44:09 +0100

    clean code

--- day-05/src/bin/part1.rs
+++ day-05/src/bin/part1.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 fn main() {
     let mut input = include_str!("input_test.txt")
         .split("\n\n");
@@ -13,17 +15,28 @@ fn main() {
         .collect::<Vec<u32>>();
 
     let maps = input.map(|m| m
-        .split_once(":\n")
-        .unwrap()
-        .1
-        .split('\n')
-        .map(|l| l
-            .split_whitespace()
-            .map(|n| n.parse().unwrap())
-            .collect::<Vec<u32>>()
+            .split_once(":\n")
+            .unwrap()
+            .1
+            .split('\n')
+            .map(|l| l
+                .split_whitespace()
+                .map(|n| n.parse().unwrap())
+                .collect::<Vec<u32>>()
+            )
+            .collect::<Vec<_>>()
         )
-        .collect::<Vec<_>>()
-    ).collect::<Vec<_>>();
+        .map(expand_map)
+        .collect::<Vec<_>>();
 
     println!("{seeds:?} {maps:?}");
+}
+
+fn expand_map(maps: Vec<Vec<u32>>) -> HashMap<u32, u32> {
+    let init = (0..100).map(|i| (i, i)).collect::<HashMap<u32, u32>>();
+    maps.into_iter().fold(init, |mut acc, m| {
+        let [value, start, len] = m[..] else { panic!("No 3 values") };
+        acc.extend((0..len).map(|i| (start+i, value+i)));
+        acc
+    })
 }
