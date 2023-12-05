e2906cdcac8ad145fac9900421cdbb7698958934
Vincent HAYS <vincenthays95@gmail.com>
2023-12-05 20:20:42 +0100

    clean code

--- day-05/src/bin/part1.rs
+++ day-05/src/bin/part1.rs
@@ -1,6 +1,29 @@
-use regex::Regex;
-
 fn main() {
-    let re = Regex::new(r"\s+").unwrap();
+    let mut input = include_str!("input_test.txt")
+        .split("\n\n");
 
+    let seeds = input
+        .next()
+        .unwrap()
+        .split_once(": ")
+        .unwrap()
+        .1
+        .split_whitespace()
+        .map(|s| s.parse().unwrap())
+        .collect::<Vec<u32>>();
+
+    let maps = input.map(|m| m
+        .split_once(":\n")
+        .unwrap()
+        .1
+        .split('\n')
+        .map(|l| l
+            .split_whitespace()
+            .map(|n| n.parse().unwrap())
+            .collect::<Vec<u32>>()
+        )
+        .collect::<Vec<_>>()
+    ).collect::<Vec<_>>();
+
+    println!("{seeds:?} {maps:?}");
 }
