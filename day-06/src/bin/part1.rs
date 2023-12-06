b35b8f44bb8be84b251d72112b544d4099ab43b2
Vincent HAYS <vincenthays95@gmail.com>
2023-12-06 13:07:23 +0100

    day 6

--- day-06/src/bin/part1.rs
+++ day-06/src/bin/part1.rs
@@ -6,10 +6,9 @@ fn main() {
     let re = Regex::new(r"\s+").unwrap();
     let [time, distance] = include_str!("input_test.txt")
         .split('\n')
-        .map(|l| l.split_once(':').unwrap().1.trim())
         .map(|l|
             re.split(l)
-            .map(|v| v.parse().unwrap())
+            .filter_map(|v| v.parse().ok())
             .collect::<Vec<u32>>()
         )
         .next_chunk::<2>()
