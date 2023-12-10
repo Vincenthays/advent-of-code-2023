fe0e40987a4f7773f16f99b37059977e98bab45f
Vincent HAYS <vincenthays95@gmail.com>
2023-12-10 22:31:26 +0100

    clean code

--- day-08/src/bin/part2.rs
+++ day-08/src/bin/part2.rs
@@ -3,11 +3,10 @@ use std::collections::HashMap;
 
 fn main() {
     let re = Regex::new(r#"[A-Z]{3}"#).unwrap();
-    let mut input = include_str!("input.txt").lines();
+    let mut input = include_str!("input_test.txt").lines().filter(|&l| l != "");
 
     let instructions = input.next().unwrap().chars().collect::<Vec<_>>();
     let network = input
-        .filter(|&l| l != "")
         .map(|l| {
             let mut nodes = re.find_iter(l).map(|m| m.as_str());
             (nodes.next().unwrap(), (nodes.next().unwrap(), nodes.next().unwrap()))
@@ -16,8 +15,8 @@ fn main() {
 
     let starts = network
         .keys()
-        .copied()
         .filter(|k| k.ends_with('A'))
+        .copied()
         .collect::<Vec<_>>();
 
     println!("{instructions:?} {network:?} {starts:?}");
@@ -31,6 +30,7 @@ fn get_step_count<'a>(instructions: Vec<char>, network: HashMap<&str, (&'a str, 
     let mut count = 0;
     for i in instructions.iter().cycle() {
         count += 1;
+        println!("{starts:?}");
         for n in starts.iter_mut() {
             *n = match i {
                 'L' => network[n].0,
