d4a14cbd41f315de25591e93460c20b6115ec5fa
Vincent HAYS <vincenthays95@gmail.com>
2023-12-11 11:13:10 +0100

    clean code

--- day-08/src/bin/part2.rs
+++ day-08/src/bin/part2.rs
@@ -54,9 +54,7 @@ fn gcd(first: u64, second: u64) -> u64 {
     let mut max = first;
     let mut min = second;
     if min > max {
-        let val = max;
-        max = min;
-        min = val;
+        (min, max) = (max, min);
     }
 
     loop {
@@ -64,7 +62,6 @@ fn gcd(first: u64, second: u64) -> u64 {
         if res == 0 {
             return min;
         }
-
         max = min;
         min = res;
     }
